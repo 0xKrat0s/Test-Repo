223eb27a7e9fdba382380db32268c33f88f14620
0xKrat0s <amalpkkratos@gmail.com>
2025-03-19 12:19:40 +0530

    Uploading source code of sports application

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// No need to import defineNuxtConfig as it's globally available in Nuxt 3
+// import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  // https://github.com/nuxt-themes/alpine
+  extends: '@nuxt-themes/alpine',
+
+  modules: [
+    // https://github.com/nuxt-modules/plausible
+    '@nuxtjs/plausible',
+    // https://github.com/nuxt/devtools
+    '@nuxt/devtools'
+  ],
+  
+  // Google Analytics and Search Console Configuration
+  app: {
+    head: {
+      meta: [
+        // Replace the content value with your Google Search Console verification code
+        { name: 'google-site-verification', content: 'YOUR_GOOGLE_VERIFICATION_CODE' }
+      ],
+      script: [
+        {
+          // Replace G-XXXXXXXXXX with your actual Google Analytics measurement ID
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXXXXX');
+          `,
+          type: 'text/javascript'
+        },
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX',
+          async: true
+        }
+      ]
+    }
+  }
+})
